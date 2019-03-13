24e073a0754d72c202a5f691ea39872fdd0c2733
Joaquin Alonzo <joaquinalonzo22@yahoo.com>
2019-03-12 22:06:35 -0700

    Rework of Planet Selection Pending: working on lock levels

---
+++ Galactic Conquest 2D/Assets/Scripts/LevelSelectScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectScript : MonoBehaviour
+{
+    private int worldIndex;
+    private int levelIndex;
+
+    void Start()
+    {
+        //loop thorugh all the worlds
+        for (int i = 1; i <= LevelLock.worlds; i++)
+        {
+            if (Application.loadedLevelName == "World" + i)
+            {
+                worldIndex = i;
+                CheckLockedLevels();
+            }
+        }
+    }
+
+    //Level to load on button click. Will be used for Level button click event 
+    public void Selectlevel(string worldLevel)
+    {
+        Application.LoadLevel("Level" + worldLevel); //load the level
+    }
+
+    //uncomment the below code if you have a main menu scene to navigate to it on clicking escape when in World1 scene
+    /*public void  Update (){
+     if (Input.GetKeyDown(KeyCode.Escape) ){
+      Application.LoadLevel("MainMenu");
+     }   
+    }*/
+
+    //function to check for the levels locked
+    void CheckLockedLevels()
+    {
+        //loop through the levels of a particular world
+        for (int j = 1; j < LevelLock.levels; j++)
+        {
+            levelIndex = (j + 1);
+            if ((PlayerPrefs.GetInt("level" + worldIndex.ToString() + ":" + levelIndex.ToString())) == 1)
+            {
+                GameObject.Find("LockedLevel" + (j + 1)).active = false;
+                Debug.Log("Unlocked");
+            }
+        }
+    }
+}
